e66ecc9fb265317dd24eab76eb9d5d171ce7353b
Jimmy Kurian <kurianjimmy11@gmail.com>
2020-07-26 18:30:09 -0500

    feat(application): add required interfaces and packages in application layer #4

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
