417a6ae184ea79066141db0b0c73b199918f0c09
Jimmy Kurian <kurianjimmy11@gmail.com>
2020-07-26 18:30:09 -0500

    feat(application): add required interfaces and packages in application layer #4

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Application;
 
 namespace WebApi
 {
@@ -20,6 +21,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApplication();
 
             #region Swagger
             services.AddSwaggerGen(c =>
